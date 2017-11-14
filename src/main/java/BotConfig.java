67aa4cbddea7a7a3f0bed3403d8969d32f63c330
tEmperror <vorobey2796@gmail.com>
2017-11-14 14:46:17 +0300

    Delete config

--- src/main/java/BotConfig.java
+++
@@ -1,4 +0,0 @@
-class BotConfig {
-    static final String TOKEN = "493736292:AAEF2tOtgoh9srEVG7hj26v3Du__Yn7gUwc";
-    static final String USERNAME = "ClashRoyale_RusBot";
-}
--- src/main/java/Metrika/BotanConfig.java
+++
@@ -1,5 +0,0 @@
-package Metrika;
-
-public class BotanConfig {
-    public final static String APIKEY = "e3db53ef-b4ff-40ad-bb57-00a860cd0d04";
-}
